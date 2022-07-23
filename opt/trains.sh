9c08cd0608a4730d7ad5f0afd7c4bf5bd72d70c2
Mark Ruvald Pedersen <mped@oticon.com>
2022-07-24 01:25:33 +0200

    Add optional script with reduced output for XFCE genmon
    Genmon lies in the panel bar, periodcally runs a command
    and display its stdout.
    
    There isn't a lot of room there, so reduce it.

---
+++ opt/trains.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+export PATH="$PATH:$HOME/.nix-profile/bin"
+
+{
+  nix-shell ~/topics/catch_the_train/shell.nix --pure --run "python3 catch_the_train/__init__.py"
+} | awk 'NR>=2{print $1, $3}'
+
