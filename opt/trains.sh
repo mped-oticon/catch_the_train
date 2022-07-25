25cde1193cde16e28b5d678b3a30551070d3f8e4
Mark Ruvald Pedersen <mped@demant.com>
2022-07-25 19:15:02 +0200

    trains.sh: Fix: Show at most next 2 departures

--- opt/trains.sh
+++ opt/trains.sh
@@ -4,5 +4,5 @@ export PATH="$PATH:$HOME/.nix-profile/bin"
 
 {
   nix-shell shell.nix --pure --run "python3 catch_the_train/__init__.py"
-} | awk 'NR>=2{print $1, $3}'
+} | sort | awk 'NR<=2{print $1, $3}'
 
